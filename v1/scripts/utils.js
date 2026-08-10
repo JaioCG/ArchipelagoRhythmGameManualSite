6abf6f201c627c2afb11548734bd8fe0308a60a4
Jacob Paccione <jaio0701@gmail.com>
2026-08-10 12:51:47 -0400

    add old v1 page for push purposes, and support Sekai

---
+++ v1/scripts/utils.js
@@ -0,0 +1,35 @@
+export function elementFromHTML(html) {
+	const template = document.createElement('template');
+	template.innerHTML = html.trim();
+	return template.content.firstChild;
+}
+
+
+export function sortTable(table) {
+	// Sort the table rows based on whether the button is disabled or not, and then by song name
+	const rows = Array.from(table.rows).slice(1); // Skip the header row
+	rows.sort((a, b) => {
+		const aButton = a.querySelector('button');
+		const bButton = b.querySelector('button');
+		if (aButton.disabled && !bButton.disabled) return 1;
+		if (!aButton.disabled && bButton.disabled) return -1;
+		return aButton.textContent.localeCompare(bButton.textContent);
+	});
+
+	// Append the sorted rows back to the table
+	rows.forEach(row => table.appendChild(row));
+}
+
+
+export function formatDifficulties(song, colors) {
+	let html = ''
+
+	song.difficulties.forEach((diff, index) => {
+		if (diff != null) {
+			const color = colors[index] || '#FFFFFF'; // Default to white if no color is specified
+			html += `<span style="color: ${color};">${diff}</span>, `;
+		}
+	});
+	
+	return html.slice(0, -2); // Remove the last comma and space
+}
